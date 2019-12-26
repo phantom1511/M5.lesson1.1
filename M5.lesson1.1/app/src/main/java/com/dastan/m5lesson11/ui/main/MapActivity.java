a06beb367c99bbdfd980806ba7c6cb353010ecca
phantom1511 <dastan.tulokulov@gmail.com>
2019-12-26 18:14:17 +0600

    home work 5.2

--- M5.lesson1.1/app/src/main/java/com/dastan/m5lesson11/ui/main/MapActivity.java
+++ M5.lesson1.1/app/src/main/java/com/dastan/m5lesson11/ui/main/MapActivity.java
@@ -3,7 +3,9 @@ package com.dastan.m5lesson11.ui.main;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,18 +15,44 @@ import android.widget.Toast;
 
 import com.dastan.m5lesson11.R;
 import com.dastan.m5lesson11.base.BaseMapActivity;
+import com.dastan.m5lesson11.service.ForegroundService;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.nabinbhandari.android.permissions.PermissionHandler;
+import com.nabinbhandari.android.permissions.Permissions;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
 
 public class MapActivity extends BaseMapActivity {
 
+    @BindView(R.id.fab)
+    FloatingActionButton floatingActionButton;
+
+    private boolean btn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        floatingActionButton.setOnClickListener(v -> {
+            if (btn == false){
+                btn = true;
+                Intent intent = new Intent(getApplicationContext(), ForegroundService.class);
+                startService(intent);
+                floatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_stop_black_24dp));
+            } else {
+                Intent intent = new Intent(getApplicationContext(), ForegroundService.class);
+                stopService(intent);
+                btn = false;
+                floatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_start));
+            }
+        });
     }
 
     @Override
